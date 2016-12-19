c21e8d79c291526c753c2160a01a86e89f255ea0
sanket9192 <gets.sanket.patel@gmail.com>
2016-12-19 17:18:30 -0600

    Update MostAppropriateFacility.R

--- MostAppropriateFacility.R
+++ MostAppropriateFacility.R
@@ -1,9 +1,18 @@
+# The core process in this assignment is to find the distance between two zip codes based on the langitudes and latitudes provided in the Dataset.
+# Each row in The Dataset contains the information of Zipcodes in terms of City, States, Longitudes and Latitudes(Approximately).
+# Using longitudes and latitudes we can calculate the approx distance between to zip codes using the Haversine formula (hf)
+# FInd the minimum distance or the nearest facility from the customer zip code will be the given result.
+# Have not considerd and inclusion exclusion rule because of the time constraint.
+# Also it is made for one customer but can be done using vector of the cutomer IDs and postan code provided.
+
 require(xlsx)
-'set working directory to load the database file'
+#'set working directory to load the database file'
 setwd("C:/Users/Sanket Patel/Desktop/copart_challenge")
+
+#read the xls file and load it to the dataframe
 t=read.xlsx2("zip_code_database.xls", sheetName = "zip_code_database")
 
-# Calculates the geodesic distance between two points specified by radian latitude/longitude using the
+# Function that Calculates the geodesic distance between two points specified by radian latitude/longitude using the
 # Haversine formula (hf)
 gcd.hf <- function(long1, lat1, long2, lat2) {
   R <- 6371 # Earth mean radius [km]
@@ -14,20 +23,27 @@ gcd.hf <- function(long1, lat1, long2, lat2) {
   d = R * c
   return(d) # Distance in km
 }
-#Assume that copart is present at following location
+
+
+#Assume that copart Facility is present at following location
+# Took the sample of Approximate 100 rows from the dataset and considered as the COpart Facility location
 set.seed(2)
 Number = sample(1:nrow(t),nrow(t)/420)
 copartLocationData = t[Number,]
 
-#Customer 1 location
+#Assume that Customer 1 give this location. Took out the langitude and latitude to pass to the function.
 myLocation="90210"
 result = t[which(t$zip==myLocation),]
 myLat=paste(result$latitude)
 myLong=paste(result$longitude)
 
+#Apply the function of calculating the distance from the longitude and latitude on the each copart facility data.
 distance = mapply(gcd.hf,as.numeric(myLong),as.numeric(myLat),as.numeric(copartLocationData$longitude),as.numeric(copartLocationData$latitude));
-copartLocationData$distance <- distance
-#gcd.hf(as.numeric(myLong),as.numeric(myLat),as.numeric(copartLong),as.numeric(copartLat))
 
+#Append the result to the data.
+copartLocationData$distance <- distance
+
+#Find the minimum distance so that the suggestion can be made to the customer.
 copartLocationData[which.min(copartLocationData$distance),c(2,3,6)]
 
+# can be done on multiple customers to find the minimum distance of for all the customers and giving suggestions to them.
