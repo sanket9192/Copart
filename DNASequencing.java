6a0474fc660dc13b3bea3c5ef567f12d1c3c5316
sanket9192 <gets.sanket.patel@gmail.com>
2016-12-19 16:57:32 -0600

    Update DNASequencing.java

--- DNASequencing.java
+++ DNASequencing.java
@@ -10,12 +10,16 @@ public class DNASequencing {
 		char maxPrimitive=DNAchars[1];
 		int maxCount=1;
 		
+		//loop through all the characters
 		for(int i = 0; i < DNAchars.length-1; i++)
 		{
+			//If two consecutive chars comes together then process
 			if(DNAchars[i]==DNAchars[i+1])
 			{
 				currentCount=currentCount+1;
 				currentPrimitive=DNAchars[i];
+				//Assign the maximum occured primitive to the different variable.
+				// Replace if found the current is bigger than older record.
 				if(maxCount < currentCount)
 				{
 					maxCount = currentCount;
@@ -26,6 +30,7 @@ public class DNASequencing {
 				currentCount=1;
 			}			
 		}
+		//Print the output
 		System.out.println(Character.toString((char) maxPrimitive)+" "+Integer.toString(maxCount));	
 	}
 
